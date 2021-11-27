8e0ca5e31fda800ad8700f3a40c1ce2bcc9e4759
Harry <hdhillon478@gmail.com>
2021-11-27 20:16:44 +0000

    Fix errors with projects views and urls

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
-    path('project/<str:pk>', views.project, name='project'),
+    path('project/<str:pk>/', views.project, name='project'),
 ]
