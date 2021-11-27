66d24b140913069b971d37540cf03b0a434a4261
Harry <hdhillon478@gmail.com>
2021-11-27 20:06:27 +0000

    Add views for projects and project to project app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def projects(request):
+    return(HttpResponse, 'This is the ALL projects page')
+
+
+def project(request, pk):
+    return(HttpResponse, 'This is a SINGLE project page' + ' ' + str(pk))
