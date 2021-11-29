35d5fb44dd381c8e5ef08cb2e8cfc6128778823d
Harry <hdhillon478@gmail.com>
2021-11-29 21:18:08 +0000

    Add Tag model

--- projects/models.py
+++ projects/models.py
@@ -30,3 +30,13 @@ class Review(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
