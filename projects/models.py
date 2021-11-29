6ef8d777bc4163eaf1a1b53be2665750d071191b
Harry <hdhillon478@gmail.com>
2021-11-29 21:23:00 +0000

    Add vote_total and vote_ratio to Project model

--- projects/models.py
+++ projects/models.py
@@ -8,6 +8,8 @@ class Project(models.Model):
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
