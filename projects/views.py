437c5ac1713214249285029ec07fd29288023c7d
Harry <hdhillon478@gmail.com>
2021-11-29 12:43:09 +0000

    Fix template route in views, Remove un-used import

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
