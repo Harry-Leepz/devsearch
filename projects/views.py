ebefe9e0e7c07825b12c293ead56eb9dcc897535
Harry <hdhillon478@gmail.com>
2021-11-30 12:54:51 +0000

    Add context and query data to views

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .models import Project
 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+    }
+
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()   # Return all the tags related to a single project
+    context = {
+        'project': project,
+        'tags': tags,
+    }
+    return render(request, 'projects/single-project.html', context)
