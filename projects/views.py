ee59561b8a9f4514f2709194c966025c455bfa16
Harry <hdhillon478@gmail.com>
2021-12-05 12:02:43 +0000

    Add view + url path for deleteProject

--- projects/views.py
+++ projects/views.py
@@ -49,3 +49,11 @@ def updateProject(request, pk):
         'form': form
     }
     return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    context = {
+        'object': project
+    }
+    return render(request, 'projects/delete_template.html', context)
