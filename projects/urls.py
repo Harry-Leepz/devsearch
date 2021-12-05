095f65f938fb255db9596c024ae98c07562d3ae2
Harry <hdhillon478@gmail.com>
2021-12-05 11:43:55 +0000

    Add url path for updateProject + link in template

--- projects/urls.py
+++ projects/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
     path('create-project/', views.createProject, name='create-project'),
+    path('update-project/<str:pk>/', views.updateProject, name='update-project'),
 ]
